23c6d7d419530c4efbd9abde784518fb55be1ab4
panyaphat insawang <68011743@kmitl.ac.th>
2026-03-25 11:19:17 +0700

    p01.4

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from './product.interface';
-import product from '../../data/products.json'
+import product from '../../data/products.json';
 import { ApiResponse } from '../interfaces/response.interface';
 
 @Injectable()
-
 export class ProductService {
-    findAll(): ApiResponse<Product[]>{
+  findAll(): ApiResponse<Product[]> {
     return {
-    success: true,
-    data: product,
-    message: "Fetched products successfully"
-          }
-    }
+      success: true,
+      data: product,
+      message: 'Fetched products successfully',
+    };
+  }
 }
-
