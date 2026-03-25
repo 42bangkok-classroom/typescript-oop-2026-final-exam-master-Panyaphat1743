3493194effe4a3b2a95581ddf7be0646a15afa60
panyaphat insawang <68011743@kmitl.ac.th>
2026-03-25 09:56:59 +0700

    p00

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ApiResponse } from './interfaces/response.interface'
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+
+export class AppService{
+  getdata(): ApiResponse<{service:string,version:string}>{
+    return {
+      success: true,
+      data: {
+        service: "purchase-api",
+        version: "1.0.0"
+      },
+      message: "Hello NestJS"
+    }
+}
 }
