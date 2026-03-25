58becea40fad745a594f6109f763f15a29938129
panyaphat insawang <68011743@kmitl.ac.th>
2026-03-25 11:12:06 +0700

    p00.nop01

---
+++ src/products/product.service.ts
@@ -0,0 +1,17 @@
+// import { Injectable } from '@nestjs/common';
+// import { Product } from './product.interface';
+// import product from '../../data/products.json'
+// import { ApiResponse } from '../interfaces/response.interface';
+
+// @Injectable()
+
+// export class ProductService {
+//     findAll(): ApiResponse<Product[]>{
+//     return {
+//     success: true,
+//     data: product,
+//     message: "Fetched products successfully"
+//           }
+//     }
+// }
+
